175f37f1c0b8e9be373fd6aad882860954293a16
Issac <lim_527@naver.com>
2021-07-07 09:14:51 +0900

    2021-07-07 static forder create, HTML CSS UPDATE

--- Project1/settings.py
+++ Project1/settings.py
@@ -132,6 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/',
+    '/static/CSS',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

59ecf93e2c4fda04874ee938c5a225f546393545
Issac <lim_527@naver.com>
2021-07-12 14:53:59 +0900

    2021-07-12 GET, POST DB Data output

--- acountapp/views.py
+++ acountapp/views.py
@@ -2,10 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from acountapp.models import HelloWorld
+
+
 def hello_world(request):
-    if request.method == 'POST' :
-        return render(request, 'acountapp/hello_world.html'
-                      ,context = {"text" : "POST METHOD"})
+    if request.method == 'POST':
+
+        temp = request.POST.get("hwt")
+        n_hw = HelloWorld()
+        n_hw.text = temp
+        n_hw.save()
+        HelloWorld_list = HelloWorld.objects.all()
+
+        return render(request, 'acountapp/hello_world.html',
+                      context={"HelloWorld_list": HelloWorld_list})
     else:
-        return render(request, 'acountapp/hello_world.html'
-                      ,context = {"text" : "GET METHOD"})
+        HelloWorld_list = HelloWorld.objects.all()
+        return render(request, 'acountapp/hello_world.html',
+                      context={"HelloWorld_list": HelloWorld_list})

84c81c7c2f441c9ef145f4bd73e2f7ed9bf03623
Issac <lim_527@naver.com>
2021-07-12 15:17:04 +0900

    2021-07-12 GET, POST Redirect Option Update

--- acountapp/views.py
+++ acountapp/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from acountapp.models import HelloWorld
+from django.urls import reverse
 
 
 def hello_world(request):
@@ -12,10 +13,14 @@ def hello_world(request):
         n_hw = HelloWorld()
         n_hw.text = temp
         n_hw.save()
-        HelloWorld_list = HelloWorld.objects.all()
 
-        return render(request, 'acountapp/hello_world.html',
-                      context={"HelloWorld_list": HelloWorld_list})
+        # 앱기반 라우팅 가능 (url을 직접 적어주지 않고 사용)
+        # 이름을 기반으로 주소로 변환해주는(역추적) 함수 사용
+        return HttpResponseRedirect(reverse("acountapp:hw"))
+
+        # HttpResponseRedirect를 통해 재연결을 해줄거임
+        # return render(request, 'acountapp/hello_world.html',
+        #               context={"HelloWorld_list": HelloWorld_list})
     else:
         HelloWorld_list = HelloWorld.objects.all()
         return render(request, 'acountapp/hello_world.html',

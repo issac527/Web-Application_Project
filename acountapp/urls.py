e92be66e04771c3347e61834a6d5eb477445ace6
Issac <lim_527@naver.com>
2021-07-15 16:06:00 +0900

    2021-07-15 Detail, Update Function Create detail.html, update.html Create

--- acountapp/urls.py
+++ acountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from acountapp.views import hello_world, AccountCreateView
+from acountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'acountapp'
 
@@ -10,5 +10,7 @@ urlpatterns = [
     path('hw/', hello_world, name = "hw"),
     path("create/", AccountCreateView.as_view(), name = "create"),
     path("login/", LoginView.as_view(template_name = "acountapp/login.html"), name = "login"),
-    path("logout", LogoutView.as_view(), name = "logout")
+    path("logout", LogoutView.as_view(), name = "logout"),
+    path("detail/<int:pk>", AccountDetailView.as_view(), name="detail"),
+    path("update/<int:pk>", AccountUpdateView.as_view(), name="update")
 ]

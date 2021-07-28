1bcffc0cfdf9d6093c08e3c561b0e44377e3b01c
Issac <lim_527@naver.com>
2021-07-28 09:33:22 +0900

    2021-07-26 ProfileUpdateView, update.html Create

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profile'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name="create")
+    path('create/', ProfileCreateView.as_view(), name="create"),
+    # pk라는 int 타입의 데이터를 넘겨 받는다
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name="update"),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -14,6 +14,18 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('acountapp:hw')
     template_name = 'profileapp/create.html'
 
+    # 유저 직접할당
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    # 어떤 것을 입력받아 수정할건지
+    form_class = ProfileCreationForm
+    # HTML에서 불러오는 이름
+    context_object_name = 'target_profile'
+    # 업데이트 후 어디로 갈지 정해줌
+    success_url = reverse_lazy('acountapp:hw')
+    # 어떤 템플릿에서 사용할지 정해줌
+    template_name = 'profileapp/update.html'

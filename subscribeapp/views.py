91e8cde6b94d4acf714547aad4e7a4fb87f91d00
Issac <lim_527@naver.com>
2021-08-23 15:12:04 +0900

    2021-08-23 SubscriptionListView , list.html Create

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 from django.urls import reverse
@@ -31,3 +32,17 @@ class SubscriptionView(RedirectView):
     # 요청 처리 후 어디로 갈지 설정
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk':kwargs['project_pk']})
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    # 구독프로젝트의 게시글을 보여줄 거기 때문에 Article 호출
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        # 현재 사용자의 구독 정보를 가져와 project List만 걸러냄
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list

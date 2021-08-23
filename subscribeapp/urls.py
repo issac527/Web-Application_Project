92fb47f87e38d6e88eeb8507f66d5f58e45a754f
Issac <lim_527@naver.com>
2021-08-23 15:12:04 +0900

    2021-08-23 SubscriptionListView , list.html Create

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscriptionListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
     # 어느 게시판에 구독정보를 보낼건지 설정
-    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe')
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
